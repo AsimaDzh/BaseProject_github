55c3a6d28b48797117370a15d249097a6e624d19
Lemonka <animasoon@gmail.com>
2026-02-01 17:32:02 +0500

    Added all input actions

--- Assets/_Scripts/Core/InputManager.cs
+++ Assets/_Scripts/Core/InputManager.cs
@@ -1,6 +1,54 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class InputManager : MonoBehaviour
 {
-    
+    public static InputManager Instance { get; private set; }
+    public InputActionAsset inputActions;
+
+    // Action Maps
+    private InputActionMap playerActionMap;
+    private InputActionMap uiActionMap;
+
+    // Player Actions
+    private InputAction moveAction;
+    private InputAction lookAction;
+    private InputAction jumpAction;
+    private InputAction attackAction;
+    private InputAction interactAction;
+    private InputAction sprintAction;
+    private InputAction crouchAction;
+    private InputAction pauseAction;
+    private InputAction cancelAction;
+
+    public Vector2 MoveInput { get; private set; }
+    public Vector2 LookInput { get; private set; }
+    public bool JumpPressed { get; private set; }
+    public bool AttackPressed { get; private set; }
+    public bool InteractPressed { get; private set; }
+    public bool SprintHeld { get; private set; }
+    public bool CrouchHeld { get; private set; }
+
+    public System.Action OnJumpPressed;
+    public System.Action OnAttackPressed;
+    public System.Action OnInteractPressed;
+    public System.Action OnPausePressed;
+    public System.Action OnCancelPressed;
+
+
+    public void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else Destroy(gameObject);
+    }
+
+    private void Start()
+    {
+        InitializeInputSystem();
+    }
 }
+
