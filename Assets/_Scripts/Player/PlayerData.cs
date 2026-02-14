f0f3a1596ef0b9857abda1b92a474a644fdafd75
Lemonka <animasoon@gmail.com>
2026-02-14 17:48:48 +0500

    Added Player data

---
+++ Assets/_Scripts/Player/PlayerData.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+// This is a ScriptableObject that can be used to store player-related data
+[CreateAssetMenu(
+    fileName = "PlayerData", 
+    menuName = "GameData/PlayerData", 
+    order = 0)]
+public class PlayerData : ScriptableObject
+{
+    [Header("========== Player Stats ==========")]
+    [Min(1f)] public float MaxHealth = 100f;
+    [Min(0f)] public float MaxMana = 0f;
+
+    [Header("========== Movement ===========")]
+    [Min(0f)] public float MoveSpeed = 5f;
+    [Min(0f)] public float JumpForce = 5f;
+
+    [Header("========== Combat ===========")]
+}
