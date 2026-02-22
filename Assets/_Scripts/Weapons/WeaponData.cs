5f88ed2c864b032e293462a19b0e85daa62172b1
Lemonka <animasoon@gmail.com>
2026-02-22 17:49:43 +0500

    Edit: Add variables to WeaponData

--- Assets/_Scripts/Weapons/WeaponData.cs
+++ Assets/_Scripts/Weapons/WeaponData.cs
@@ -1,10 +1,35 @@
 using UnityEngine;
 
+
 [CreateAssetMenu(
     fileName = "WeaponData", 
-    menuName = "Game Data/WeaponData", 
-    order = 0)]
+    menuName = "GameData/WeaponData", 
+    order = 1)]
+
 public class WeaponData : MonoBehaviour
 {
+    public enum WeaponType
+    {
+        Melee = 0,
+        Ranged = 1,
+        Magic = 2
+    }
 
+    [Header("========== General ==========")]
+    public string weaponName = "New Weapon";
+    public WeaponType weaponType = WeaponType.Melee;
+    public Sprite icon;
+
+    [Header("========== Attack ==========")]
+    [Min(0f)] public float damage = 10f;
+    [Min(0.1f)] public float attackSpeed = 1f;
+    [Min(0f)] public float range = 2f;
+    [Min(0f)] public float knockbackForce = 0f;
+
+    [Header("========== Animations & Effects ==========")]
+    public string attackAnimationName;
+    public AudioClip attackSound;
+
+    [Header("========== Projectiles ===========")]
+    public GameObject projectilePrefab;
 }
