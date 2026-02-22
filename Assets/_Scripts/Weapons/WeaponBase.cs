9a9f007c78472625d40762f0a264d10ec53913ec
Lemonka <animasoon@gmail.com>
2026-02-22 18:31:06 +0500

    Add: Create variables & methods in WeaponBase

--- Assets/_Scripts/Weapons/WeaponBase.cs
+++ Assets/_Scripts/Weapons/WeaponBase.cs
@@ -1,6 +1,37 @@
 using UnityEngine;
 
-public class WeaponBase : MonoBehaviour
+public abstract class WeaponBase : MonoBehaviour
 {
+    [Header("========== Weapon Data ==========")]
+    [SerializeField] private WeaponData weaponData;
+    [SerializeField] private Transform owner;
 
+    protected float nextAttackTime = 0f;
+
+    public float Damage => weaponData.damage;
+    public float Range => weaponData.range;
+    public float AttackSpeed => weaponData.attackSpeed;
+
+
+    public virtual bool CanAttack()
+    {
+        if (weaponData == null)
+        {
+            Debug.LogWarning($"{name}: WeaponData not assigned", this);
+            return false;
+        }
+
+        return Time.time >= nextAttackTime;
+    }
+
+
+    protected void StartAttackCooldown()
+    {
+        // if AttackSpeed is 0 or negative, we can set a default cooldown to prevent spamming attacks every frame
+        float cooldown = AttackSpeed > 0f ? (1f / AttackSpeed) : 0.5f;
+        nextAttackTime = Time.time + cooldown;
+    }
+
+
+    public abstract void Attack();
 }
